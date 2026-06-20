0b38993407e92c1c47615001e1c738f4961bf50d
timeandspace-m <dg288yw6pb@privaterelay.appleid.com>
2026-06-20 17:21:00 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
 // ★あとで作成するGASの「WebアプリURL」を入れる場所です（一旦空欄でOK）
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzX5F4hjFGmVMZSrH8kV_XFgzWVnriOSnV92IYgxChTjsP5t6U-M55XGS_juHcKIGtL/exec"; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyV8At7iDrjUfHJsF3F2e6tTIOxYYTxMBYUQNsJ_yDEOFnmXvgPuprhV7Wj5hdSJTXC/exec"; 
 
 // フォームの「登録する」ボタンが押されたときの動き（プッシュ通知専用）
 document.getElementById('registration-form').addEventListener('submit', async (e) => {
